d89c568e6ed92f54b83d50efeadbefb678e8b432
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2025-01-13 18:44:58 +0100

    init10

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,15 +1,15 @@
 import Post from './Post';
 import classes from "./PostsList.module.css";
-
+import NewPost from './NewPost';
 function PostsList() {
   return (
+    <>
+    <NewPost/>
     <ul className={classes.posts}>
       < Post author='Maximilian' body="React je dobar" />
       < Post author='Manuel' body="Dobar kurs" />
     </ul>
-
-    
-
+    </>
   );
 }
 export default PostsList;
