6456d8379eccf535b859763319fe1f7023eaa40b
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2025-01-13 21:22:39 +0100

    init100

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,27 @@
 import classes from './NewPost.module.css';
-
-function NewPost() {
+import { useState } from 'react';
+function NewPost(props) {
+  const [enteredBody,setEnteredBody]=useState('');
+  const [enteredAuthor,setEnterAuthor]=useState('');
+  function changeBodyHandler(event){
+    // console.log(event.target.value);
+    setEnteredBody(event.target.value);
+   }
+    function changeAuthorHandler(event){
+      // console.log(event.target.value);
+      setEnteredAuthor(event.target.value);
+   }
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required  onChange={props.onAuthorChange}/>
       </p>
+    
     </form>
   );
 }
